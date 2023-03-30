37b2b36231ccb0db99aa4700c2bee097f881f6b9
vijay-inexture <vijaydaraji@inexture.com>
2023-03-30 15:50:54 +0530

    hello message

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,7 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
+		
+		System.out.println("Hello");
 		SpringApplication.run(DemoApplication.class, args);
+		
 	}
 
 }
