05fabd11d20ef3321d5fe26deed98f3bf1753896
vijay-inexture <vijaydaraji@inexture.com>
2023-03-30 16:49:12 +0530

    hello vijay

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,7 +8,7 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello World");
+		System.out.println("Hello vijay");
 		SpringApplication.run(DemoApplication.class, args);
 		
 	}
